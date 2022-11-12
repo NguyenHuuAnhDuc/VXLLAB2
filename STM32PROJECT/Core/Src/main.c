ed074b2f31f411a8257bdd7b3e54e1f8796d8810
DESKTOP-7R66M1N\Admins <duc.nguyen2903@hcmut.edu.vn>
2022-11-12 22:36:20 +0700

    Init for EX2

--- STM32PROJECT/Core/Src/main.c
+++ STM32PROJECT/Core/Src/main.c
@@ -72,40 +72,40 @@ void display7SEG(int num){
 	  case 1:
 		  HAL_GPIO_WritePin(GPIOB,B_Pin|C_Pin, GPIO_PIN_RESET);
 		  HAL_GPIO_WritePin(GPIOB,A_Pin|D_Pin|E_Pin|F_Pin|G_Pin, GPIO_PIN_SET);
-	  		  break;
+	  	  break;
 	  case 2:
 		  HAL_GPIO_WritePin(GPIOB,F_Pin|C_Pin, GPIO_PIN_SET);
 		  HAL_GPIO_WritePin(GPIOB,B_Pin|G_Pin|D_Pin|E_Pin|A_Pin, GPIO_PIN_RESET);
-	  		  break;
+	      break;
 	  case 3:
 		  HAL_GPIO_WritePin(GPIOB,F_Pin|E_Pin, GPIO_PIN_SET);
 		  HAL_GPIO_WritePin(GPIOB,B_Pin|G_Pin|D_Pin|C_Pin|A_Pin, GPIO_PIN_RESET);
-	  		  break;
+	  	  break;
 	  case 4:
 		  HAL_GPIO_WritePin(GPIOB,A_Pin|E_Pin|D_Pin, GPIO_PIN_SET);
 		  HAL_GPIO_WritePin(GPIOB,B_Pin|G_Pin|F_Pin|C_Pin, GPIO_PIN_RESET);
-	  		  break;
+	  	  break;
 	  case 5:
 		  HAL_GPIO_WritePin(GPIOB,B_Pin|E_Pin, GPIO_PIN_SET);
 		  HAL_GPIO_WritePin(GPIOB,F_Pin|G_Pin|D_Pin|C_Pin|A_Pin, GPIO_PIN_RESET);
-	  		  break;
+	      break;
 	  case 6:
 		  HAL_GPIO_WritePin(GPIOB,B_Pin, GPIO_PIN_SET);
 		  HAL_GPIO_WritePin(GPIOB,F_Pin|G_Pin|D_Pin|E_Pin|C_Pin|A_Pin, GPIO_PIN_RESET);
-	  		  break;
+	  	  break;
 	  case 7:
 		  HAL_GPIO_WritePin(GPIOB,F_Pin|E_Pin|D_Pin|G_Pin, GPIO_PIN_SET);
 		  HAL_GPIO_WritePin(GPIOB,B_Pin|C_Pin|A_Pin, GPIO_PIN_RESET);
-	  		  break;
+	  	  break;
 	  case 8:
 		  HAL_GPIO_WritePin(GPIOB,B_Pin|C_Pin|D_Pin|E_Pin|F_Pin|A_Pin|G_Pin, GPIO_PIN_RESET);
-	  		  break;
+	  	  break;
 	  case 9:
 		  HAL_GPIO_WritePin(GPIOB,E_Pin, GPIO_PIN_SET);
 		  HAL_GPIO_WritePin(GPIOB,B_Pin|C_Pin|D_Pin|G_Pin|F_Pin|A_Pin, GPIO_PIN_RESET);
-	  		  break;
+	  	  break;
 	  default:
-	  		  break;
+		  break;
 	  }
 
 }
