2ed1cd2ca2723f14b01f310d38202ca010e3e598
NguyenHuuAnhDuc <anhducnguyen.1803@gmail.com>
2022-11-13 00:18:32 +0700

    Init for EX7

--- STM32PROJECT/Core/Src/main.c
+++ STM32PROJECT/Core/Src/main.c
@@ -60,56 +60,46 @@ void display7SEG(int num){
 	switch (num){
 	  case 0:
 		  HAL_GPIO_WritePin(GPIOB,G_Pin, GPIO_PIN_SET);
-		  HAL_GPIO_WritePin(GPIOB,B_Pin|C_Pin|D_Pin
-		                           |E_Pin|F_Pin|A_Pin, GPIO_PIN_RESET);
-		  break;
+		  HAL_GPIO_WritePin(GPIOB,B_Pin|C_Pin|D_Pin|E_Pin|F_Pin|A_Pin, GPIO_PIN_RESET);
+		      break;
 	  case 1:
 		  HAL_GPIO_WritePin(GPIOB,B_Pin|C_Pin, GPIO_PIN_RESET);
-		  HAL_GPIO_WritePin(GPIOB,A_Pin|D_Pin
-		  		                           |E_Pin|F_Pin|G_Pin, GPIO_PIN_SET);
+		  HAL_GPIO_WritePin(GPIOB,A_Pin|D_Pin|E_Pin|F_Pin|G_Pin, GPIO_PIN_SET);
 	  		  break;
 	  case 2:
 		  HAL_GPIO_WritePin(GPIOB,F_Pin|C_Pin, GPIO_PIN_SET);
-		  HAL_GPIO_WritePin(GPIOB,B_Pin|G_Pin|D_Pin
-		  		                           |E_Pin|A_Pin, GPIO_PIN_RESET);
+		  HAL_GPIO_WritePin(GPIOB,B_Pin|G_Pin|D_Pin|E_Pin|A_Pin, GPIO_PIN_RESET);
 	  		  break;
 	  case 3:
 		  HAL_GPIO_WritePin(GPIOB,F_Pin|E_Pin, GPIO_PIN_SET);
-		  HAL_GPIO_WritePin(GPIOB,B_Pin|G_Pin|D_Pin
-		  		  		                   |C_Pin|A_Pin, GPIO_PIN_RESET);
+		  HAL_GPIO_WritePin(GPIOB,B_Pin|G_Pin|D_Pin|C_Pin|A_Pin, GPIO_PIN_RESET);
 	  		  break;
 	  case 4:
 		  HAL_GPIO_WritePin(GPIOB,A_Pin|E_Pin|D_Pin, GPIO_PIN_SET);
-		  HAL_GPIO_WritePin(GPIOB,B_Pin|G_Pin|F_Pin
-		  		  		  		           |C_Pin, GPIO_PIN_RESET);
+		  HAL_GPIO_WritePin(GPIOB,B_Pin|G_Pin|F_Pin|C_Pin, GPIO_PIN_RESET);
 	  		  break;
 	  case 5:
 		  HAL_GPIO_WritePin(GPIOB,B_Pin|E_Pin, GPIO_PIN_SET);
-		  HAL_GPIO_WritePin(GPIOB,F_Pin|G_Pin|D_Pin
-		  		  		  		           |C_Pin|A_Pin, GPIO_PIN_RESET);
+		  HAL_GPIO_WritePin(GPIOB,F_Pin|G_Pin|D_Pin|C_Pin|A_Pin, GPIO_PIN_RESET);
 	  		  break;
 	  case 6:
 		  HAL_GPIO_WritePin(GPIOB,B_Pin, GPIO_PIN_SET);
-		  HAL_GPIO_WritePin(GPIOB,F_Pin|G_Pin|D_Pin|E_Pin
-		  		  		  		  		   |C_Pin|A_Pin, GPIO_PIN_RESET);
+		  HAL_GPIO_WritePin(GPIOB,F_Pin|G_Pin|D_Pin|E_Pin|C_Pin|A_Pin, GPIO_PIN_RESET);
 	  		  break;
 	  case 7:
 		  HAL_GPIO_WritePin(GPIOB,F_Pin|E_Pin|D_Pin|G_Pin, GPIO_PIN_SET);
 		  HAL_GPIO_WritePin(GPIOB,B_Pin|C_Pin|A_Pin, GPIO_PIN_RESET);
 	  		  break;
 	  case 8:
-		  HAL_GPIO_WritePin(GPIOB,B_Pin|C_Pin|D_Pin
-		 		                           |E_Pin|F_Pin|A_Pin|G_Pin, GPIO_PIN_RESET);
+		  HAL_GPIO_WritePin(GPIOB,B_Pin|C_Pin|D_Pin|E_Pin|F_Pin|A_Pin|G_Pin, GPIO_PIN_RESET);
 	  		  break;
 	  case 9:
 		  HAL_GPIO_WritePin(GPIOB,E_Pin, GPIO_PIN_SET);
-		  HAL_GPIO_WritePin(GPIOB,B_Pin|C_Pin|D_Pin
-		  		                           |G_Pin|F_Pin|A_Pin, GPIO_PIN_RESET);
+		  HAL_GPIO_WritePin(GPIOB,B_Pin|C_Pin|D_Pin|G_Pin|F_Pin|A_Pin, GPIO_PIN_RESET);
 	  		  break;
 	  default:
 	  		  break;
 	  }
-
   }
 
 
